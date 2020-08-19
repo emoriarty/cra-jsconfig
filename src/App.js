9bdae87a432796f0c0171b409825930028b9e55c
Enrique <emoriarty81@gmail.com>
2020-08-19 17:27:04 +0200

    jsconfig

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "components/Alpha";
+import Alpha from "components/Alpha";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Alpha />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Alpha.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Bold from "helpers/bold";
+
+export default function Alpha() {
+  return <Bold>Alpha</Bold>;
+}
